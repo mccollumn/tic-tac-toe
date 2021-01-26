46edb7df2bee9517feaeeba569b8982431f45eb3
Nick McCollum <nmccollum81@gmail.com>
2021-01-26 10:28:23 -0800

    Bold selected item, and rewrite Board to avoid hardcoded squares.

--- src/index.js
+++ src/index.js
@@ -21,23 +21,20 @@ class Board extends React.Component {
   }
 
   render() {
+    const row = 3;
+    const col = 3;
+    let HTML = [];
+    let i = 0;
+    for (let r = 0; r < row; r++) {
+      HTML.push(<div className="board-row"></div>);
+      for (let c = 0; c < col; c++, i++) {
+        HTML.push(this.renderSquare(i));
+      }
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {HTML}
       </div>
     );
   }
@@ -49,6 +46,8 @@ class Game extends React.Component {
     this.state = {
       history: [{
         squares: Array(9).fill(null),
+        player: 'X',
+        placement: null,
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -66,6 +65,8 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        player: squares[i],
+        placement: i + 1,
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -85,12 +86,18 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
+      const placement = calculatePosition(step.placement - 1, this.state.history[move].player);
+      const isSelected = this.state.stepNumber === move ? true : false;
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + ' ' + placement :
         'Go to game start';
+      const buttonText = isSelected ? [<b>{desc}</b>] : [desc];
+
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}>
+            {buttonText}
+          </button>
         </li>
       );
     });
@@ -144,4 +151,13 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+function calculatePosition(index, player) {
+  const rowLength = 3;
+  let col, row = null;
+  col = (index % rowLength) + 1;
+  row = Math.floor(index / rowLength) + 1;
+
+  return "(" + player + " to " + col + "," + row + ")";
 }
